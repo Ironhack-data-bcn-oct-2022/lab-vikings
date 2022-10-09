5bc1ab6ad6d8ced9dca9a2997b323a9cda37beaa
Selma <slimaa.l3@gmail.com>
2022-10-09 19:57:08 +0200

    vikingLab

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -2,20 +2,61 @@
 # Soldier
 
 
-class Soldier:
-    pass
+class Soldier():
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+
+    def attack(self):
+        return self.strength
+
+    def receiveDamage (self, damage):
+       self.health -= damage
 
 # Viking
 
+class Viking(Soldier):
+    def __init__(self,name,health,strength):
+        self.name=name
+        super().__init__(health,strength)
 
-class Viking:
-    pass
+    def receiveDamage(self, damage):
+        self.health-=damage
+        if self.health > 0:
+            return f"{self.name} has received {damage} points of damage"
+        else:
+            return f"{self.name} has died in act of combat"
+
+    def battleCry(self):
+        return "Odin Owns You All!"
+       
 
 # Saxon
 
+class Saxon(Soldier):
+    def __init__(self,health,strength):
+        super().__init__(health,strength)
 
-class Saxon:
-    pass
+    def receiveDamage(self,damage):
+        self.health-=damage
+        if self.health > 0:
+            return f"A Saxon has received {damage} points of damage"
+        else:
+            return "A Saxon has died in combat"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # War
 
